8f5d6c64a0c5f3b7230027d024a739022897eca8
Leandro Elias Goerck <lgoerck@universo.univates.br>
2022-08-15 16:46:08 -0300

    req 2 - add test_relatorio_produto

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,25 @@
-# from inventory_report.inventory.product import Product
+import string
+from typing import Type
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    relatorio_do_produto = Product(
+        1,
+        "Controle de Xbox",
+        "Microsoft",
+        "15/08/2022",
+        "15/08/2023",
+        "123456",
+        "em local seco",
+    )
+
+    assert str(relatorio_do_produto) == (
+    f"O produto Controle de Xbox"
+    f" fabricado em 15/08/2022"
+    f" por Microsoft"
+    f" com validade até 15/08/2023"
+    f" precisa ser armazenado"
+    F" em local seco."
+)
+
